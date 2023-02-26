e1825090a7085923315e325ad8c3da8c2356f9db
Shibbaz <kmosc@protonmail.com>
2023-02-26 18:44:53 +0100

    application config changes

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,7 @@ module Loop
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.autoloader = :classic
+    config.eager_load_paths += Dir[Rails.root.join('app/contexts/**/*.rb')].each { |rb| require rb }
   end
 end
